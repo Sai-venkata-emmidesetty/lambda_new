9e6244760a2e7c54a069ec14464a57cfeb1680ef
Venkata Sai Emmidesetty <141021513+Sai-venkata-emmidesetty@users.noreply.github.com>
2025-02-09 09:14:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,4 @@
-terraform {
-  required_providers {
-    aws ={
-        source = "hashicorp/aws"
-        version = "~>5.0"
-    }
-  }
-}
 
-provider "aws" {
-  region = "us-east-1" # Needs to be filled in with a specific AWS region
-
-  # Configuration options can be added here if needed, for example:
-  # access_key = ""
-  # secret_key = ""
-}
 
 module "lambda" {
   source = "./modules/lambda"
