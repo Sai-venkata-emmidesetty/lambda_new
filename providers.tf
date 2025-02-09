a196f431389e30af9861e2797ec28633d29747fe
Venkata Sai Emmidesetty <141021513+Sai-venkata-emmidesetty@users.noreply.github.com>
2025-02-09 09:15:06 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    aws ={
+        source = "hashicorp/aws"
+        version = "~>5.0"
+    }
+  }
+}
+
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-1" # Needs to be filled in with a specific AWS region
+
+  # Configuration options can be added here if needed, for example:
+  # access_key = ""
+  # secret_key = ""
 }
