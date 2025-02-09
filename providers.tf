1d45348b2133b12decd9c2574b16a08511f1fe33
Venkata Sai Emmidesetty <141021513+Sai-venkata-emmidesetty@users.noreply.github.com>
2025-02-09 09:19:59 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -8,9 +8,5 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1" # Needs to be filled in with a specific AWS region
-
-  # Configuration options can be added here if needed, for example:
-  # access_key = ""
-  # secret_key = ""
+  region = "us-east-1" 
 }
