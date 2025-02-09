5f32b13c36384a53b20564770b330b1c5ea139e6
Venkata Sai Emmidesetty <141021513+Sai-venkata-emmidesetty@users.noreply.github.com>
2025-02-09 09:50:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,5 +2,5 @@ module "lambda" {
   source = "./modules/lambda"
   lambda_name = "dev_env_snowflake" 
   s3_bucket = "edl-mig" 
-  s3_key = "edl-mig/global_pay/bronze/app.zip" 
+  s3_key = "s3://edl-mig/global_pay/bronze/app.zip" 
 }
